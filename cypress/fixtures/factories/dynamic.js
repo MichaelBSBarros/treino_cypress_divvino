5028b38d15b21fec747a041e81537208af84c5e2
MichaelBSBarros <75203020+MichaelBSBarros@users.noreply.github.com>
2021-08-06 13:41:10 -0300

    atualização

--- cypress/fixtures/factories/dynamic.js
+++ cypress/fixtures/factories/dynamic.js
@@ -44,7 +44,7 @@ export default class DynamicFactory {
                 "nomeContato"   : `${faker.name.firstName()} ${faker.name.lastName()}`,
                 "razaoSocial"   : faker.company.companyName(),
                 //"ie_num"        : faker.datatype.number({'min': 111111111, 'max': 999999999}),
-                ie_num          : faker.random.arrayElement(ie_num),
+                "ie_num"          : faker.random.arrayElement(ie_num),
                 "email"         : faker.internet.email(),
                 "senha"         : faker.internet.password(),
                 "celular"       : faker.phone.phoneNumber('8199#######')
